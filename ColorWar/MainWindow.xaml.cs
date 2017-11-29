fafc2ac5290c38bc06cd2757f2a9551a344c0197
rmr6894 <rmr6894@rit.edu>
2017-11-29 15:25:26 -0500

    first commit

---
+++ ColorWar/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorWar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            double width = mainPanel.Width / 5;
+            double height = mainPanel.Height / 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    mainPanel.Children.Add(new Button
+                    {
+                        Width = width,
+                        Height = height
+                    });
+                }
+            }
+            //this.SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        //private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    this.SizeToContent = SizeToContent.WidthAndHeight;
+        //}
+    }
+}
